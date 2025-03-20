1231197d5e138ce5708514d176889f254789e407
Myht <dev@myhtica.com>
2025-03-20 09:36:46 -0700

    Changed threshold folder to line-threshold

--- src/test/test-files/threshold/long.tsx
+++
@@ -1,4 +0,0 @@
-// src/test/test-files/threshold/long.tsx
-<div className="flex items-center justify-between p-4 m-2 text-lg font-bold bg-blue-500 hover:bg-blue-600 rounded-lg shadow-md">
-  Over threshold example
-</div>;
--- src/test/test-files/threshold/short.tsx
+++
@@ -1,2 +0,0 @@
-// src/test/test-files/threshold/short.tsx
-<div className="text-lg p-4 m-2">Under threshold example</div>;
